3fd5dbd92976a85edf7c352ad0072714e08e9eaf
Kishor Prins <kprinssu@uwaterloo.ca>
2015-06-06 08:25:38 -0400

    Added noble for BLE

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var noble = require('noble');
 
 //enable express to use body-parse to handle POST data
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -9,4 +10,6 @@ app.use(bodyParser.json());
 //get our port
 var port = process.env.PORT || 3000;
 
+//routes
 var router = express.Router();
+
